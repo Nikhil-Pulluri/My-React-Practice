8cfe98ed177a9ffe396a2a197cf6080b11f9f0c5
Nikhil Pulluri <nikhilpulluri7810@gmail.com>
2024-07-15 20:52:05 +0530

    made a todo list app using context api

--- 10todo_list_using_contextapi/src/App.jsx
+++ 10todo_list_using_contextapi/src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { TodoProvider } from './context'
+import { TodoForm, TodoItems } from './components/index'
 
 function App() {
 
@@ -10,10 +11,42 @@ function App() {
     setTodos((prev) => [{id: Date.now(), ...todo},...prev])
   }
 
+  const updateTodo = (id, todo) => {
+    setTodos((prev)=> prev.map((prevTodo)=> (prevTodo.id === todo.id? todo : prevTodo)))
+  }
+
+  const deleteTodo = (id) => {
+    setTodos((prev)=> prev.filter((todo)=> todo.id !== id))
+  }
+
+  const toggleCompleted = (id) => {
+    setTodos((prev)=> prev.map((prevTodo)=> prevTodo.id===id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo))
+  }
+
+
+
+  // for getting the list form the local storage
+  useEffect(()=> {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+    if(todos && todos.length > 0)
+    {
+      setTodos(todos)
+    }
+  }, [])
+
+
+  // for setting the things inside the local storage
+  useEffect(()=> {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
 
   return (
     <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleCompleted}}>
-      <h1 className='text-3xl font-bold underline'>Hello there!</h1>
+      <TodoForm/>
+      {
+        todos.map((todo)=>( 
+        <div key={todo.id}><TodoItems todo = {todo} /></div>))
+      }
     </TodoProvider>
   )
 }
